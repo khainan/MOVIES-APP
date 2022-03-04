8688370de902650c5fb12f41cca1f0275e8c6726
khainan <muhammadrachim.k@gmail.com>
2022-03-04 12:10:43 +0700

    chore: add like, dislike, and views on movie card

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import Image from 'next/image';
 import homeIcon from '../public/assets/home-icon.png';
 import starIcon from '../public/assets/star-icon.png';
 import ratingIcon from '../public/assets/rating-icon.png';
+import likeIcon from '../public/assets/like-icon.png';
+import viewsIcon from '../public/assets/views-icon.png';
+import dislikeIcon from '../public/assets/dislike-icon.png';
 
 // components
 import HeaderSidebar from '../src/components/HeaderSidebar';
@@ -65,10 +68,21 @@ const Home: NextPage<IHomeProps> = ({ movies }) => {
                   style={{ backgroundImage: `url(${movie.imageUrl})` }}
                 >
                   <div className={styles['movie-card-header']}>
-                    <h4>{movie.title}</h4>
+                    <h4>{`${movie.title} (${movie.year})`}</h4>
                     <div className={styles['movie-card-rating']}>
                       <Image alt="" src={ratingIcon} width={16} height={16} />
                       <p>{`x${movie.rating}`}</p>
+                    </div>
+                  </div>
+                  <div className={styles['movie-card-content']}>
+                    <div className={styles['eye-icon']}>
+                      <Image alt="" src={viewsIcon} width={24} height={24} />
+                    </div>
+                    <div className={styles['like-icon']}>
+                      <Image alt="" src={likeIcon} width={24} height={24} />
+                    </div>
+                    <div className={styles['dislike-icon']}>
+                      <Image alt="" src={dislikeIcon} width={24} height={24} />
                     </div>
                   </div>
                 </div>
