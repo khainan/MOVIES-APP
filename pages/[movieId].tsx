e02df18d8de7903f24e9ff518035c47237bcba36
khainan <muhammadrachim.k@gmail.com>
2022-03-06 18:11:17 +0700

    chore: fix error build

--- pages/[movieId].tsx
+++ pages/[movieId].tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
-import Router, { withRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import { useState, useEffect, useCallback } from 'react';
 import Head from 'next/head';
 
 // assets
@@ -13,8 +13,26 @@ import HeaderSidebar from '../src/components/HeaderSidebar';
 // styles
 import styles from '../styles/Home.module.scss';
 
-const DetailMoviePage: NextPage = () => {
+// utils
+import { translate } from '../src/utils/translate';
+
+type Movies = {
+  id: string;
+  title: string;
+  year: number;
+  rating: number;
+  imageUrl: string;
+};
+
+interface IFavoriteProps {
+  movies: Movies[];
+}
+
+
+const DetailMoviePage: NextPage<IFavoriteProps> = () => {
   const [movieData, setMovieData] = useState('');
+  const [movieLists, setMovieLists] = useState([]);
+  const [language, setLanguage] = useState('en');
 
   const menus = [
     {
@@ -31,24 +49,51 @@ const DetailMoviePage: NextPage = () => {
     },
   ];
 
+  const handleGetLikes = useCallback(() => {
+    const likes: any = localStorage.getItem('likes') || [];
+    const parsedLikes = likes.length ? JSON.parse(likes) : [];
+
+    setMovieLists(parsedLikes);
+  }, []);
+
+  const handleChangeLanguage = (lang: string) => {
+    setLanguage(lang);
+  };
+
+  const handleUpdateMovie = (movie: any) => {
+    setMovieLists(movie);
+  };
+
   useEffect(() => {
     const movie = localStorage.getItem('movieData');
+    
     if (movie) {
       setMovieData(movie);
     } else {
       Router.back();
     }
-  }, []);
+
+    localStorage.setItem('lang', language);
+    handleGetLikes();
+  }, [language, handleGetLikes]);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Movie App</title>
-        <meta name="description" content="Movie app" />
+        <title>Favorite</title>
+        <meta name="description" content="Favorite Page by Movie App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HeaderSidebar menus={menus}>
-        <div>{movieData}</div>
+      <HeaderSidebar
+        menus={menus}
+        language={language}
+        onChangeLanguage={(lang: string) => handleChangeLanguage(lang)}
+      >
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            {movieData}
+          </div>
+        </div>
       </HeaderSidebar>
     </div>
   );
